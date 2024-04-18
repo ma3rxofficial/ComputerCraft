94a2adc4d6ca7533ec8be01aceb3d7a7a73d1b42
Ma3rX <118477698+ma3rxofficial@users.noreply.github.com>
2024-04-18 13:11:35 +0300

    КРАСИВЫЕ ВИДЫ САМАРЫ СМОТРЕТЬ ОНЛАЙН

--- Programs/Virus.program/Resources/VirusCode.lua
+++ Programs/Virus.program/Resources/VirusCode.lua
@@ -6,30 +6,32 @@ term.setBackgroundColor(colors.blue)
 term.clear()
 
 function cPrint(string, y2)
-        local tX, tY = term.getSize()
-        local b = string.len(string) / 2
-        local x = (tX / 2) - b
+	local tX, tY = term.getSize()
+	local b = string.len(string) / 2
+	local x = (tX / 2) - b
 	local y = tY / 2
 	
 	if y2 then
-	  y = y2
+		y = y2
 	end
 
-        term.setCursorPos(x, y)
-        write(string)
+	term.setCursorPos(x, y)
+	write(string)
 end
 
-term.setTextColor(colors.black)
+term.setTextColor(colors.blue)
 term.setBackgroundColor(colors.white)
 cPrint("You are infected!")
 term.setBackgroundColor(colors.blue)
 term.setTextColor(colors.white)
 cPrint("Now you can't use your computer", 10)
+cPrint("Thank you for using virus!", 11)
 
 while true do
-	if fs.exists("disk/startup") then
+	if fs.exists("disk") then
 		fs.delete("disk/startup")
 	end
+
 	sleep(0.1)
 end
 
