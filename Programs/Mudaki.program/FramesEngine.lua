5eba3d799064996d4896b4d04656ff4cfd4c2d1c
Ma3rX <118477698+ma3rxofficial@users.noreply.github.com>
2024-04-27 14:21:13 +0300

    поясняю

--- Programs/Mudaki.program/FramesEngine.lua
+++ Programs/Mudaki.program/FramesEngine.lua
@@ -1,16 +1,16 @@
-kadr_quantity = 4
-kadr = 1
-
+kadr_quantity = 4 -- сколько всего кадров
+kadr = 1 -- с какого кадра начнем воспрозведение
+ 
 while true do
   event, key = os.pullEvent()
   
-  if event == "key" and key == keys.right then
+  if event == "key" and key == keys.right then -- следующий кадр
     if kadr < kadr_quantity then
       kadr = kadr + 1
     else
       kadr = 1
     end
-  elseif event == "key" and key == keys.left then
+  elseif event == "key" and key == keys.left then -- предыдущий кадр
     if kadr == 1 then
       kadr = kadr_quantity
     else
@@ -18,7 +18,7 @@ while true do
     end
   end
   
-  if event == "mouse_click" then
+  if event == "mouse_click" then  -- следующий кадр но с мышки
     if kadr < kadr_quantity then
       kadr = kadr + 1
     else
@@ -26,5 +26,5 @@ while true do
     end
   end
   
-  print("FRAME "..tostring(kadr))
+  print("FRAME "..tostring(kadr)) -- печатаем какой щас кадр
 end
