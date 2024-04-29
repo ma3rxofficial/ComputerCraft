63a3f0c2f83f04c22d9dba081a83afa4b9ecebef
Ma3rX <118477698+ma3rxofficial@users.noreply.github.com>
2024-04-29 17:42:25 +0300

    КОЛЕСИКО МЫШИ

--- Programs/Mudaki.program/FramesEngine.lua
+++ Programs/Mudaki.program/FramesEngine.lua
@@ -4,13 +4,13 @@ kadr = 1 -- с какого кадра начнем воспрозведение
 while true do
   event, key = os.pullEvent()
   
-  if event == "key" and key == keys.right or event == "mouse_click" and key == 1 or event == "mouse_drag" and key == 1 then -- следующий кадр
+  if event == "key" and key == keys.right or event == "mouse_click" and key == 1 or event == "mouse_drag" and key == 1 or event == "mouse_scroll" and key == -1 then -- следующий кадр
     if kadr < kadr_quantity then
       kadr = kadr + 1
     else
       kadr = 1
     end
-  elseif event == "key" and key == keys.left or event == "mouse_click" and key == 2 or event == "mouse_drag" and key == 2 then -- предыдущий кадр
+  elseif event == "key" and key == keys.left or event == "mouse_click" and key == 2 or event == "mouse_drag" and key == 2 or event == "mouse_scroll" and key == 1 then -- предыдущий кадр
     if kadr == 1 then
       kadr = kadr_quantity
     else
