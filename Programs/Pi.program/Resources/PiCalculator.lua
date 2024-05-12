d09e924c0d1816e84578210b86244d7d614e1256
Ma3rX <118477698+ma3rxofficial@users.noreply.github.com>
2024-05-12 05:56:25 +0300

    версия для пастебина

---
+++ Programs/Pi.program/Resources/PiCalculator.lua
@@ -0,0 +1,39 @@
+--[[
+    The program calculates Pi 
+    using the following formula: 4/1 - 4/3 + 4/5 - 4/7 +...
+]]
+
+logPath = "Pi.log"
+
+function InfoLog(msg)
+    log_file = fs.open(logPath, "a")
+    log_file.writeLine(msg)
+    log_file.flush()
+    log_file.close()
+end
+
+iL = 1000
+b = 1
+c = 0
+i = 1
+pi = 0
+
+while true do
+    pi = pi + 4/b
+    b = b + 2
+    pi = pi - 4/b
+    b = b + 2
+    c = c + 1
+
+    message = "Iteration: "..tostring(i).." Sub-iteration: "..tostring(c).." Pi: "..tostring(pi)
+
+    print(message)
+    InfoLog(message)
+    
+    if c == iL then
+        c = 0
+        i = i + 1
+    end
+
+    sleep(0)
+end
