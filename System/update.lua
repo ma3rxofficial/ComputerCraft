9459cf2d4fd8634c857336135a70843a72134ff6
Ma3rX <118477698+ma3rxofficial@users.noreply.github.com>
2024-06-08 08:26:44 +0300

    Update update.lua

--- System/update.lua
+++ System/update.lua
@@ -1,36 +1,4 @@
 
------------------------------------------------------------------------------------------------------------------------------------
--- Функции скачивания
-
-function downloadBlob(v)
-	if isBlacklisted(Settings.InstallPath..v.path) then -- если файл в блеклисте
-		return -- то хуй вам, а не файл!
-	end
-	if v.type == 'tree' then -- если тип этого файла - это дерево(т. е. он папка)
-		print('Making folder: '..'/.update/'..Settings.InstallPath..v.path, 'Making folder: '..'/.update/'..Settings.InstallPath..v.path) -- печатаем, шо создаем папку
-		fs.makeDir('/.update/'..Settings.InstallPath..v.path) -- само создание папки
-	else
-		print('(' .. Settings.DownloadedBytes .. 'B/' .. Settings.TotalBytes .. 'B) Downloading file: '..Settings.InstallPath..v.path, Settings.InstallPath..v.path, 'Downloading files...') --  печатаем, что файл скачивается
-		local f = http.get(('https://raw.github.com/'..Settings.GitHubUsername..'/'..Settings.GitHubRepoName..'/'..latestReleaseTag..Settings.InstallPath..v.path):gsub(' ','%%20')) -- отправляем запрос на гитхаб с путем до файла
-		if not f then -- если не получилось отправить запрос или нет ответа
-			error('Downloading failed, try again. '..('https://raw.github.com/'..Settings.GitHubUsername..'/'..Settings.GitHubRepoName..'/'..latestReleaseTag..Settings.InstallPath..v.path):gsub(' ','%%20')) -- печатаем, что все труба
-		end
-		local h = fs.open('/.update/'..Settings.InstallPath..v.path, 'w') -- открываем файл по пути
-		h.write(f.readAll()) -- вставляем туда содержимое файла с гитхаба
-		h.close() -- закрываем локальный файл на компьютере пользователя
-
-		if v.size then -- если у файла есть размер
-			Settings.DownloadedBytes = Settings.DownloadedBytes + v.size -- добавляем к кол-ву скачанных байт размер этого файла
-		end
-
-		Settings.DownloadedFiles = Settings.DownloadedFiles + 1 -- и также с кол-вом файлов
-		Settings.UpdateFunction() -- обновляем
-	end
-	sleep(0) -- фикс "too long without yielding"
-end
-
--- Массив загрузок
-local downloads = {}
 
 fs.makeDir('/.update/') -- создаем папку обновления
 
